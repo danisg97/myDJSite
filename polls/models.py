f0df3c502ac65da226366357101e24bfa2b95a1d
danisg97 <sanchez.gracia.daniel@gmail.com>
2018-12-03 00:16:49 +0100

    Adding comments

--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 from django.utils import timezone
 
+#Quesiton model
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -14,10 +15,13 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    #Custom Admin site
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
 
+#Choice model
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
